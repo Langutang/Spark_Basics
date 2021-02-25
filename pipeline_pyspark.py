5bc3758fd46ffa711145cf63994e894250a5bfba
John Lang <55468621+Langutang@users.noreply.github.com>
2021-02-24 22:15:23 -0600

    Create pipeline_pyspark.py

---
+++ pipeline_pyspark.py
@@ -0,0 +1,120 @@
+################
+
+# PIPELINES
+# Whole Process Walkthrough
+
+###########################
+# ENSEMBLES AND PIPELINES #
+###########################
+
+# Convert categorical strings to index values
+indexer = StringIndexer(inputCol='org', outputCol='org_idx')
+
+# One-hot encode index values
+onehot = OneHotEncoderEstimator(
+    inputCols=['org_idx', 'dow'],
+    outputCols=['org_dummy', 'dow_dummy']
+)
+
+# Assemble predictors into a single column
+assembler = VectorAssembler(inputCols=['km', 'org_dummy', 'dow_dummy'], outputCol='features')
+
+# A linear regression object
+regression = LinearRegression(labelCol='duration')
+
+# Import class for creating a pipeline
+from pyspark.ml import Pipeline
+
+# Construct a pipeline
+pipeline = Pipeline(stages=[indexer, onehot, assembler, regression])
+
+# Train the pipeline on the training data
+pipeline = pipeline.fit(flights_train)
+
+# Make predictions on the testing data
+predictions = pipeline.transform(flights_test)
+
+################
+# TOKENIZATION #
+################
+
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF
+
+# Break text into tokens at non-word characters
+tokenizer = Tokenizer(inputCol='text', outputCol='words')
+
+# Remove stop words
+remover = StopWordsRemover(inputCol='words', outputCol='terms')
+
+# Apply the hashing trick and transform to TF-IDF
+hasher = HashingTF(inputCol="terms", outputCol="hash")
+idf = IDF(inputCol="hash", outputCol="features")
+
+# Create a logistic regression object and add everything to a pipeline
+logistic = LogisticRegression()
+pipeline = Pipeline(stages=[tokenizer, remover, hasher, idf, logistic])
+
+
+####################
+# CROSS VALIDATION #
+####################
+
+# Create an empty parameter grid
+params = ParamGridBuilder().build()
+
+# Create objects for building and evaluating a regression model
+regression = LinearRegression(labelCol='duration')
+evaluator = RegressionEvaluator(labelCol='duration')
+
+# Create a cross validator
+cv = CrossValidator(estimator=regression, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+# Train and test model on multiple folds of the training data
+cv = cv.fit(flights_train)
+
+# Create an indexer for the org field
+indexer = StringIndexer(inputCol='org', outputCol='org_idx')
+
+# Create an one-hot encoder for the indexed org field
+onehot = OneHotEncoderEstimator(inputCols=['org_idx'], outputCols=['org_dummy'])
+
+# Assemble the km and one-hot encoded fields
+assembler = VectorAssembler(inputCols=['km', 'org_dummy'], outputCol='features')
+
+# Create a pipeline and cross-validator.
+pipeline = Pipeline(stages=[indexer, onehot, assembler, regression])
+cv = CrossValidator(estimator=pipeline,
+                    estimatorParamMaps=params,
+                    evaluator=evaluator)
+
+###############
+# GRID SEARCH #
+###############
+
+# Create parameter grid
+params = ParamGridBuilder()
+
+# Add grids for two parameters
+params = params.addGrid(regression.regParam, [0.01, 0.1, 1.0, 10.0]) \
+               .addGrid(regression.elasticNetParam, [0.0, 0.5, 1.0])
+
+# Build the parameter grid
+params = params.build()
+print('Number of models to be tested: ', len(params))
+
+# Create cross-validator
+cv = CrossValidator(estimator=pipeline, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+# Best Model
+# Get the best model from cross validation
+best_model = cv.bestModel
+
+# Look at the stages in the best model
+print(best_model.stages)
+
+# Get the parameters for the LinearRegression object in the best model
+best_model.stages[3].extractParamMap()
+
+# Generate predictions on testing data using the best model then calculate RMSE
+predictions = best_model.transform(flights_test)
+evaluator.evaluate(predictions)
